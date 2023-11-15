54fa7440c1eab245ec5b574c17327d20ace47724
Seth <sethyanow@users.noreply.github.com>
2023-11-15 16:21:33 -0500

    feat: setup vcs automation

--- locals.tf
+++ locals.tf
@@ -7,7 +7,9 @@ locals {
   workspace = {
     "fem-eci-tfe" = {
       description = "Example description of workspace"
+      execution_mode = "remote"
       project_id  = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "sethyanow/fem-eci-terraform-tfe"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
     default = "sethyanow"
     type = string
 }
+
+variable "github_app_installation_id" {
+    default = 44061378
+    type = number
+}
+
+variable "github_organization_name" {
+    default = "sethyanow"
+    type = string
+}
