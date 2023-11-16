b40b3484efdbf53b8dc69a9f5cf5390e2286caac
Seth <sethyanow@users.noreply.github.com>
2023-11-16 13:01:44 -0500

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -6,10 +6,16 @@ locals {
   }
   workspace = {
     "fem-eci-tfe" = {
-      description = "Example description of workspace"
-      execution_mode = "remote"
-      project_id  = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "sethyanow/fem-eci-terraform-tfe"
+      description         = "Example description of workspace"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}fem-eci-terraform-tfe"
+    }
+    "fem-eci-github" = {
+      description         = "Example description of workspace"
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
     }
   }
 }
