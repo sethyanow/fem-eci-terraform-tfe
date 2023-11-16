f9307a7dbc762bd14e619b29105f603e91f626a6
Seth <sethyanow@users.noreply.github.com>
2023-11-16 14:09:06 -0500

    feat: make github sync

--- locals.tf
+++ locals.tf
@@ -13,7 +13,7 @@ locals {
     }
     "fem-eci-github" = {
       description         = "Example description of workspace"
-      execution_mode      = "local"
+      execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
